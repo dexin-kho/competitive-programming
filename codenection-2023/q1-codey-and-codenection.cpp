98c4d405cbe7bb253dbd2d865182023873633c0a
Kho De Xin <146016480+dexin-kho@users.noreply.github.com>
2024-03-19 20:18:00 +0800

    Create q1-codey-and-codenection.cpp

---
+++ codenection-2023/q1-codey-and-codenection.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(int n) {
+	for (int i = 0; i < n; i++)
+		cout << "I LOVE CODENECTION\n";
+}
+
+int main() {
+	int n;
+	cin >> n;
+	solve(n);
+}
